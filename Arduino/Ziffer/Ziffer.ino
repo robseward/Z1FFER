6404399fad899a9d378db9da231d13dafd4cab02
Rob Seward <Rob.Seward@rga.com>
2014-07-17 12:08:09 -0400

    Refactored arduino code and added comments

--- Arduino/Ziffer/Ziffer.ino
+++ Arduino/Ziffer/Ziffer.ino
@@ -1,48 +1,48 @@
 
-#define READ_PIN 7
+byte previousByte = 0;
 
 void setup()
 {
   Serial.begin(230400);
 
-  DDRD = B00000000;
+  //Set B pins to input
   DDRB = B00000000;
   delay(1000);
 }
 
+void loop()
+{
+  takeSample();
+}
 
-byte previousByte = 0;
-  
 void takeSample()
 {
   byte pinInput1 = 0;
   byte pinInput2 = 0;
   byte combinedInput = 0;
   byte outbyte = 0;
-  byte b = 0;
+  byte currentByte = 0;
  
+  //For loop for building a byte from bits 
   for(int j=0; j < 8; j++){ 
-    //Pins are read using registers because its much faster
+    //Pins are read using registers because its much faster than digitalRead()
     pinInput1 = (PINB >> 3) & B00000001;
     pinInput2 = (PINB >> 4) & B00000001;
 
+    //Mix the two Z1FFER bits
     combinedInput = pinInput1 ^ pinInput2;
     
-    b = (combinedInput << j) | b;
+    //Add the bit in combinedInput to currentByte
+    currentByte = (combinedInput << j) | currentByte;
   }  
 
-  outbyte = b ^ previousByte;
+  //Mix the current byte with the previous one
+  outbyte = currentByte ^ previousByte;
   Serial.write(outbyte);  
   
-  previousByte = b;
-  //For eyeball debugging
-  //delay(100);
-  //Serial.println(b, BIN);
+  previousByte = currentByte;
 }  
 
-void loop()
-{
-  takeSample();
-}
+
 
 
