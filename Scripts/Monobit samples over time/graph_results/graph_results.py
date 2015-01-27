c367e6301d939bb92ffa8fc36aacc8ff18025835
Rob Seward <robseward@gmail.com>
2015-01-27 00:44:45 -0500

    Updating monobit sample graphing script to handle timestamps

--- Scripts/Monobit samples over time/graph_results/graph_results.py
+++ Scripts/Monobit samples over time/graph_results/graph_results.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plot
+import matplotlib.dates as md
+from matplotlib.dates import date2num
+import datetime
 
-f = open("deviations.csv")
+f = open("deviations2.csv")
 
 values = []
+timestamps = []
 for (i, line) in enumerate(f):
     if i >= 1:
-        value = line.split(",")[1].strip()
+    	lineArray = line.split(",")
+    	date = datetime.datetime.strptime(lineArray[0], '%Y-%m-%d %H:%M:%S')
+    	timestamps.append(date2num(date))
+        value = lineArray[1].strip()
         values.append(value)
     if i > 100000:
         break
-
-plot.plot(values)
+plot.subplots_adjust(bottom=0.2)
+plot.xticks( rotation=25 )
+ax=plot.gca()
+xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+ax.xaxis.set_major_formatter(xfmt)
+plot.plot(timestamps, values)
 plot.show()
