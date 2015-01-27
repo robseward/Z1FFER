e33eff3021060cf3b66296e5e3eb8d1d823f6258
Rob Seward <robseward@gmail.com>
2015-01-27 00:46:39 -0500

    Updating whitening algorithm

--- Arduino/Ziffer/Ziffer.ino
+++ Arduino/Ziffer/Ziffer.ino
@@ -7,6 +7,10 @@
 /******************************/
  
 byte previousByte = 0;
+byte previousMixedByte1 = 0;
+byte previousMixedByte2 = 0;
+byte previousMixedByte3 = 0;
+byte previousMixedByte4 = 0;
 
 void setup()
 {
@@ -25,25 +29,35 @@ void loop()
 void takeSample()
 {
   byte pinInput1 = 0;
-  byte outbyte = 0;
+  byte outByte = 0;
   byte currentByte = 0;
- 
+  byte mixedByte1 = 0;
+  byte mixedByte2 = 0;
+  byte mixedByte3 = 0;
+  byte mixedByte4 = 0;
+
   //Loop for building a byte from bits 
   for(int j=0; j < 8; j++){ 
     //Pins are read using registers because its much faster than digitalRead()
     pinInput1 = (PINB >> 3) & B00000001;
-    
     //Add the bit to currentByte
     currentByte = (pinInput1 << j) | currentByte;
   }  
 
   //Mix the current byte with the previous one
-  outbyte = currentByte ^ previousByte;
-  
+  mixedByte1 = currentByte ^ previousByte;
+  mixedByte2 = mixedByte1 ^ previousMixedByte1;
+  mixedByte3 = mixedByte2 ^ previousMixedByte2;
+  mixedByte4 = mixedByte3 ^ previousMixedByte3;
+  outByte = mixedByte4 ^ previousMixedByte4;
   //Send it to your computer
-  Serial.write(outbyte);  
+  Serial.write(outByte);  
   
   previousByte = currentByte;
+  previousMixedByte1 = mixedByte1;
+  previousMixedByte2 = mixedByte2; 
+  previousMixedByte3 = mixedByte3;
+  previousMixedByte4 = mixedByte4;
 }  
 
 
