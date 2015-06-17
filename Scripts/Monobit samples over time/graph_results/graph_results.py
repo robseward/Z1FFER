e4789d671cb1c4cdfea31eb76fbe690235ef5c4a
Rob Seward <robseward@gmail.com>
2015-06-16 23:30:37 -0400

    Add linear regression to sampling script

--- Scripts/Monobit samples over time/graph_results/graph_results.py
+++ Scripts/Monobit samples over time/graph_results/graph_results.py
@@ -2,16 +2,19 @@ import matplotlib.pyplot as plot
 import matplotlib.dates as md
 from matplotlib.dates import date2num
 import datetime
+# from pylab import *
+from numpy import polyfit
+import numpy as np
 
-f = open("deviations2.csv")
+f = open("20150315_deviations.csv")
 
 values = []
 timestamps = []
 for (i, line) in enumerate(f):
     if i >= 1:
-    	lineArray = line.split(",")
-    	date = datetime.datetime.strptime(lineArray[0], '%Y-%m-%d %H:%M:%S')
-    	timestamps.append(date2num(date))
+        lineArray = line.split(",")
+        date = datetime.datetime.strptime(lineArray[0], '%Y-%m-%d %H:%M:%S')
+        timestamps.append(date2num(date))
         value = lineArray[1].strip()
         values.append(value)
     if i > 100000:
@@ -21,5 +24,14 @@ plot.xticks( rotation=25 )
 ax=plot.gca()
 xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
 ax.xaxis.set_major_formatter(xfmt)
-plot.plot(timestamps, values)
+
+# countArray = np.arange(0.0, len(timestamps))
+floatValues = np.array(map(float, values))
+fit = polyfit(timestamps,floatValues,1)
+fit_fn = np.poly1d(fit) # fit_fn is now a function which takes in x and returns an estimate for y
+
+# plot(x,y, 'yo', x, fit_fn(x), '--k')
+
+plot.plot(timestamps, values, timestamps, fit_fn(timestamps), '--k')
+#plot.plot(timestamps, values)
 plot.show()
